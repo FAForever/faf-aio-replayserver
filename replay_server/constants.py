08bbb32a7070d3fe5e23cb330c6126f8e98b5c6a
Konstantin Kalinovsky <konstantin.kalinovsky@firma.seznam.cz>
2018-09-09 22:33:11 +0200

    faf-stacking project

--- replay_server/constants.py
+++ replay_server/constants.py
@@ -1,16 +1,14 @@
 import os
-from logging import INFO
 
 BASE_DIR = os.path.abspath(os.path.join(os.path.dirname(os.path.realpath(__file__)), '..', '..'))
 
 # logging
-# LOGGING_LEVEL = int(os.environ.get("LOGGING_LEVEL", DEBUG))
-LOGGING_LEVEL = int(os.environ.get("LOGGING_LEVEL", INFO))
+LOGGING_LEVEL = int(os.environ.get("LOGGING_LEVEL", 20))  # INFO = 20 DEBUG = 10 ERROR = 40
 
 # server config
 PORT = int(os.environ.get("PORT", 15000))
 # Timeout step for waiting till next stream will come.
-WAIT_STEP = 0.1  # in seconds
+WAIT_STEP = 0.1  # in seconds, it might influence fluent streaming, default is 0.1
 # Wait timeout, till we will send live stream back
 REPLAY_TIMEOUT = int(os.environ.get("REPLAY_TIMEOUT", 60 * 5))
 READ_BUFFER_SIZE = int(os.environ.get("READ_BUFFER_SIZE", 1024))
@@ -47,7 +45,7 @@ BUG_SNAG_API_KEY = os.environ.get("BUG_SNAG_API_KEY", "3305a486a2f18fdcff9512c5a
 MYSQL_DNS = {
     "host": os.environ.get("MYSQL_HOST", "127.0.0.1"),
     "port": int(os.environ.get("MYSQL_PORT", 3306)),
-    "user": os.environ.get("MYSQL_PORT", "root"),
+    "user": os.environ.get("MYSQL_USER", "root"),
     "password": os.environ.get("MYSQL_PASSWORD", "banana"),
     "db": os.environ.get("MYSQL_DB", "faf-dump"),
 }

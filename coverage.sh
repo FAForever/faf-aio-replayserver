213d51d522cf058c4026eb4c71c4eb0b3d450d16
Konstantin Kalinovsky <konstantin.kalinovsky@firma.seznam.cz>
2018-09-10 00:11:42 +0200

    Using new replay parser repository + travis configuration

--- coverage.sh
+++ coverage.sh
@@ -2,4 +2,5 @@
 pyclean .
 rm -r tmp/*
 rm -r replays/*
-STREAM_ENDED_TIMEOUT=0 python3.6 -m pytest --cov=replay_server --cov-report=html -rxs `find ./tests/ -iname "*.py"`
+
+python3.6 -m pytest --cov=replay_server --cov-report=html -rxs `find ./tests/ -iname "*.py"`
--- test.sh
+++ test.sh
@@ -2,4 +2,5 @@
 pyclean .
 rm -r tmp/*
 rm -r replays/*
-STREAM_ENDED_TIMEOUT=0 python3.6 -m pytest -rxs `find ./tests/ -iname "*.py"`
+
+python3.6 -m pytest -rxs `find ./tests/ -iname "*.py"`

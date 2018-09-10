0fdbf4629002408d03a17d8d12969b7a16816ec9
Igor Kotrasinski <i.kotrasinsk@gmail.com>
2018-09-10 17:56:15 +0200

    Add a replay stream class for strategy to write to
    Signed-off-by: Igor Kotrasinski <i.kotrasinsk@gmail.com>

--- replayserver/replaystream.py
+++ replayserver/replaystream.py
@@ -1,3 +1,6 @@
+from asyncio.locks import Condition
+
+
 class ReplayStream:
     """
     Has to be called before read_data.
@@ -23,12 +26,23 @@ class ReplayStream:
         return self.data_from(position)
 
 
-class ConnectionReplayStream(ReplayStream):
-    def __init__(self, connection):
+class ConcreteReplayStream:
+    def __init__(self):
         ReplayStream.__init__(self)
-        self._connection = connection
         self.header = None
         self.data = bytearray()
+
+    def data_length(self):
+        return len(self.data)
+
+    def data_from(self, position):
+        return self.data[position:]
+
+
+class ConnectionReplayStream(ConcreteReplayStream):
+    def __init__(self, connection):
+        ConcreteReplayStream.__init__(self)
+        self._connection = connection
         self._finished = False
 
     async def read_header(self):
@@ -40,11 +54,44 @@ class ConnectionReplayStream(ReplayStream):
             self._finished = True
         self.data += data
 
-    def data_length(self):
-        return len(self.data)
+    def is_complete(self):
+        return self._finished
 
-    def data_from(self, position):
-        return self.data[position:]
+
+class OutsideSourceReplayStream(ReplayStream):
+    def __init__(self):
+        ReplayStream.__init__(self)
+        self._finished = False
+        self._new_data = Condition()
+
+    async def _wait_for(self, condition):
+        await self._new_data.acquire()
+        await self._new_data.wait_for(condition)
+        self._new_data.release()
+
+    async def read_header(self):
+        await self._wait_for(lambda: self.header is not None
+                             or self.is_complete())
+        if self.header is None:
+            raise ValueError    # FIXME
+        return self.header
+
+    def set_header(self, header):
+        self.header = header
+        self._new_data.notify_all()
+
+    async def read(self):
+        current_len = self.data_length()
+        await self._wait_for(lambda: current_len < self.data_length()
+                             or self.is_complete())
+        return
+
+    def feed_data(self, data):
+        self.data += data
+        self._new_data.notify_all()
 
     def is_complete(self):
         return self._finished
+
+    def finish(self):
+        self._finished = True

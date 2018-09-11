cd6bc68b3fa9900157eaee615c469afcdf042e8a
Igor Kotrasinski <i.kotrasinsk@gmail.com>
2018-09-11 21:01:55 +0200

    Add setup script
    Signed-off-by: Igor Kotrasinski <i.kotrasinsk@gmail.com>

--- replayserver/main.py
+++ replayserver/main.py
@@ -2,14 +2,16 @@ import asyncio
 from replayserver import ReplayServer
 
 
-if __name__ == "__main__":
+def main():
     PORT = 15000
     server = ReplayServer.build(PORT)
     loop = asyncio.get_event_loop()
-    start = asyncio.ensure_future(server.start())
+    asyncio.ensure_future(server.start())
     try:
         loop.run_forever()
+        return 0
     except Exception:
         loop.run_until_complete(server.stop())
+        return 1
     finally:
         loop.close()
---
+++ setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup
+
+setup(
+    name="faf_replay_server",
+    version='0.0.1',
+    description="FAF replay server using asyncio",
+    long_description="TODO",
+    classifiers=[
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3.6",
+    ],
+    author="Igor Kotrasiński",
+    author_email="i.kotrasinsk@gmail.com",
+    url="https://github.com/FAForever/faf-aio-replayserver",
+    keywords="FAForever replay server",
+    license="GPL2",
+    packages=["replayserver"],
+    entry_points={
+        "console_scripts": [
+            "faf_replay_server = replayserver.main:main",
+        ],
+    },
+)

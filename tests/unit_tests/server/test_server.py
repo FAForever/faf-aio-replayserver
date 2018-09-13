a5662cbf112f658c36a5c3b57d01646eff2f6c50
Igor Kotrasinski <i.kotrasinsk@gmail.com>
2018-09-13 22:58:36 +0200

    Use pytest-asyncio
    Signed-off-by: Igor Kotrasinski <i.kotrasinsk@gmail.com>

--- tests/unit_tests/server/test_server.py
+++ tests/unit_tests/server/test_server.py
@@ -1,4 +1,5 @@
 import pytest
+import decorator
 import asyncio
 import asynctest
 from replayserver.server.server import Server
@@ -80,73 +81,67 @@ def mock_connections():
     return build
 
 
-@pytest.fixture
-def coroutine_runner():
-    def run(coro):
-        async def run_with_timeout():
-            await asyncio.wait_for(coro, 1)
-        f = asyncio.ensure_future(run_with_timeout())
-        return asyncio.get_event_loop().run_until_complete(f)
-    return run
+def timeout(time):
+    def deco(coro):
+        async def wrapper_function(coro, *args, **kwargs):
+            return (await asyncio.wait_for(coro(*args, **kwargs), time))
+        # Needed for fixtures to work
+        return decorator.decorator(wrapper_function, coro)
+    return deco
 
 
-def test_server_start(mock_replays, mock_server_maker, mock_connections,
-                      coroutine_runner):
+@pytest.mark.asyncio
+@timeout(1)
+async def test_server_start(mock_replays, mock_server_maker, mock_connections):
+    connection = mock_connections(None, None)
+    mock_connection_builder = MockConnectionBuilder([connection])
+    server = Server(mock_server_maker, mock_replays, mock_connection_builder)
+    await server.start()
+    mock_server_maker.assert_awaited_with(server.handle_connection)
+
+
+@pytest.mark.asyncio
+@timeout(1)
+async def test_server_successful_connection(mock_replays, mock_server_maker,
+                                            mock_connections):
     connection = mock_connections(None, None)
     mock_connection_builder = MockConnectionBuilder([connection])
     server = Server(mock_server_maker, mock_replays, mock_connection_builder)
 
-    async def continue_test():
-        await server.start()
-        mock_server_maker.assert_awaited_with(server.handle_connection)
-
-    coroutine_runner(continue_test())
-
-
-def test_server_successful_connection(mock_replays, mock_server_maker,
-                                      mock_connections, coroutine_runner):
-    connection = mock_connections(None, None)
-    mock_connection_builder = MockConnectionBuilder([connection])
-    server = Server(mock_server_maker, mock_replays, mock_connection_builder)
-
-    async def continue_test():
-        await server.start()
-        await server.handle_connection("reader", "writer")
-        assert connection._reader == "reader"
-        assert connection._writer == "writer"
-        connection.read_header.assert_awaited()
-        mock_replays.handle_connection.assert_awaited()
-        connection.close.assert_called()
-
-    coroutine_runner(continue_test())
+    await server.start()
+    await server.handle_connection("reader", "writer")
+    assert connection._reader == "reader"
+    assert connection._writer == "writer"
+    connection.read_header.assert_awaited()
+    mock_replays.handle_connection.assert_awaited()
+    connection.close.assert_called()
 
 
-def test_server_stopping(mock_replays, mock_server_maker,
-                         mock_connections, coroutine_runner):
-    connection = mock_connections(None, None)
+@pytest.mark.asyncio
+@timeout(1)
+async def test_server_stopping(mock_replays, mock_server_maker,
+                               mock_connections):
     read_header_called = asyncio.locks.Event()
     verified = asyncio.locks.Event()
+
+    connection = mock_connections(None, None)
+    mock_connection_builder = MockConnectionBuilder([connection])
 
     async def at_header_read():
         read_header_called.set()
         await verified.wait()
-
     connection.read_header.side_effect = at_header_read
 
-    mock_connection_builder = MockConnectionBuilder([connection])
     server = Server(mock_server_maker, mock_replays, mock_connection_builder)
 
-    async def continue_test():
-        await server.start()
-        asyncio.ensure_future(server.handle_connection("reader", "writer"))
-        await read_header_called.wait()
-        await server.stop()
+    await server.start()
+    asyncio.ensure_future(server.handle_connection("reader", "writer"))
+    await read_header_called.wait()
+    await server.stop()
 
-        mock_server = mock_server_maker.return_value
-        mock_server.close.assert_called()
-        mock_server.wait_closed.assert_awaited()
-        connection.close.assert_called()
-        mock_replays.stop.assert_awaited()
-        verified.set()
-
-    coroutine_runner(continue_test())
+    mock_server = mock_server_maker.return_value
+    mock_server.close.assert_called()
+    mock_server.wait_closed.assert_awaited()
+    connection.close.assert_called()
+    mock_replays.stop.assert_awaited()
+    verified.set()

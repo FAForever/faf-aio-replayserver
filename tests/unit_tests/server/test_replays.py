80258e34adbd70a64e65987f1ef3d831e5a8b8f5
Igor Kotrasinski <i.kotrasinsk@gmail.com>
2018-09-15 21:48:16 +0200

    Add a successful connection Replays test
    Signed-off-by: Igor Kotrasinski <i.kotrasinsk@gmail.com>

--- tests/unit_tests/server/test_replays.py
+++ tests/unit_tests/server/test_replays.py
@@ -1,6 +1,7 @@
 import pytest
 import asynctest
 from tests import timeout
+from asyncio.locks import Event
 
 from replayserver.server.connection import Connection
 from replayserver.server.replays import Replays
@@ -20,23 +21,67 @@ def mock_replay():
         async def wait_for_ended():
             pass
 
-    return asynctest.Mock(spec=R)
+    manual_replay_end = Event()
+    mr = asynctest.Mock(spec=R)
+
+    async def manual_ended_wait():
+        await manual_replay_end.wait()
+
+    ended_wait_mock = asynctest.CoroutineMock(wraps=manual_ended_wait)
+
+    mr.configure_mock(_manual_replay_end=manual_replay_end,
+                      wait_for_ended=ended_wait_mock)
+    return mr
 
 
 @pytest.fixture
-def mock_replay_builder():
+def mock_replays():
     return mock_replay
+
+
+@pytest.fixture
+def mock_replay_builder(mocker):
+    return mocker.Mock(spec=[])
 
 
 @pytest.mark.asyncio
 @timeout(1)
 async def test_reader_with_no_existing_replay_is_not_processed(
-        mock_replay_builder, mock_connections, mocker):
+        mock_connections, mock_replay_builder):
     connection = mock_connections(None, None)
     connection.type = Connection.Type.READER
     connection.uid = 1
-    replay_builder = mocker.Mock()
 
-    replays = Replays(replay_builder)
+    replays = Replays(mock_replay_builder)
     await replays.handle_connection(connection)
-    replay_builder.assert_not_called()
+    mock_replay_builder.assert_not_called()
+
+
+@pytest.mark.asyncio
+@timeout(1)
+async def test_readers_successful_connection(
+        mock_replays, mock_replay_builder, mock_connections, mocker):
+    writer = mock_connections(None, None)
+    writer.type = Connection.Type.WRITER
+    writer.uid = 1
+    reader = mock_connections(None, None)
+    reader.type = Connection.Type.READER
+    reader.uid = 1
+    mock_replay = mock_replays()
+    mock_replay_builder.side_effect = [mock_replay]
+    replays = Replays(mock_replay_builder)
+
+    await replays.handle_connection(writer)
+    mock_replay_builder.assert_called_once()
+    mock_replay.handle_connection.assert_called_with(writer)
+    mock_replay.handle_connection.reset_mock()
+    mock_replay_builder.reset_mock()
+
+    await replays.handle_connection(reader)
+    mock_replay_builder.assert_not_called()
+    mock_replay.handle_connection.assert_called_with(reader)
+    mock_replay.handle_connection.reset_mock()
+    mock_replay_builder.reset_mock()
+
+    mock_replay._manual_replay_end.set()
+    await replays.stop()

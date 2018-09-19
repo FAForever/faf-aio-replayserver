bc69ab3b4923c6a6c356cd8a9648cde5285caa46
Igor Kotrasinski <i.kotrasinsk@gmail.com>
2018-09-19 17:59:30 +0200

    Factor out common parts of header tests, more dict tests
    Signed-off-by: Igor Kotrasinski <i.kotrasinsk@gmail.com>

--- tests/unit_tests/struct/test_header.py
+++ tests/unit_tests/struct/test_header.py
@@ -7,141 +7,112 @@ from replayserver.struct import header
 
 # We assume GeneratorData works, for sake of easier testing.
 # We have unit tests for it wnyway.
-def test_read_value_endianness():
+def run_cor(cor, data, *args):
     gen = GeneratorData()
-    gen.data = b"\1\0\0\0"
-    cor = header.read_value(gen, "I", 4)
+    gen.data = data
+    cor = cor(gen, *args)
+    cor.send(None)
+
+
+def test_read_value_endianness():
     with pytest.raises(StopIteration) as v:
-        cor.send(None)
+        run_cor(header.read_value, b"\1\0\0\0", "I", 4)
     assert v.value.value == 1
 
 
 def test_read_value_struct_error():
-    gen = GeneratorData()
-    gen.data = b"\0\0\0\0\0\0"
-    cor = header.read_value(gen, "f", 2)
     with pytest.raises(ValueError):
-        cor.send(None)
+        run_cor(header.read_value, b"\0\0\0\0\0\0", "f", 2)
 
 
 def test_read_string():
-    gen = GeneratorData()
-    gen.data = b"aaaaa\0"
-    cor = header.read_string(gen)
     with pytest.raises(StopIteration) as v:
-        cor.send(None)
+        run_cor(header.read_string, b"aaaaa\0")
     assert v.value.value == "aaaaa"
 
 
 def test_read_string_invalid_unicode():
-    gen = GeneratorData()
     # Lonely start character is invalid unicode
-    gen.data = b"aaaa\xc0 \0"
-    cor = header.read_string(gen)
+    data = b"aaaa\xc0 \0"
     with pytest.raises(ValueError):
-        cor.send(None)
+        run_cor(header.read_string, data)
 
 
 def test_read_lua_type():
-    gen = GeneratorData()
-    gen.data = b"\0"
-    cor = header.read_lua_type(gen)
     with pytest.raises(StopIteration) as v:
-        cor.send(None)
-        assert v.value.value == header.LuaType.NUMBER
+        run_cor(header.read_lua_type, b"\0")
+    assert v.value.value == header.LuaType.NUMBER
 
-    gen = GeneratorData()
-    gen.data = b"\x42"
-    cor = header.read_lua_type(gen)
     with pytest.raises(ValueError):
-        cor.send(None)
+        run_cor(header.read_lua_type, b"\x42")
 
 
 def test_lua_nil_value():
-    gen = GeneratorData()
-    gen.data = b"\2"
-    cor = header.read_lua_value(gen)
     with pytest.raises(StopIteration) as v:
-        cor.send(None)
+        run_cor(header.read_lua_value, b"\2")
     assert v.value.value is None
 
 
 def test_lua_number_value():
-    gen = GeneratorData()
     number = 2.375  # Nice representable number
-    gen.data = b"\0" + struct.pack("<f", number)
-    cor = header.read_lua_value(gen)
+    data = b"\0" + struct.pack("<f", number)
     with pytest.raises(StopIteration) as v:
-        cor.send(None)
+        run_cor(header.read_lua_value, data)
     assert v.value.value == number
 
 
 def test_lua_bool_value():
-    gen = GeneratorData()
-    gen.data = b"\3\x17"
-    cor = header.read_lua_value(gen)
     with pytest.raises(StopIteration) as v:
-        cor.send(None)
+        run_cor(header.read_lua_value, b"\3\x17")
     assert v.value.value is False   # Not a typo
 
-    gen = GeneratorData()
-    gen.data = b"\3\0"
-    cor = header.read_lua_value(gen)
     with pytest.raises(StopIteration) as v:
-        cor.send(None)
+        run_cor(header.read_lua_value, b"\3\0")
     assert v.value.value is True   # Not a typo
 
 
 def test_lua_string_value():
-    gen = GeneratorData()
-    gen.data = b"\1aaaaa\0"
-    cor = header.read_lua_value(gen)
+    data = b"\1aaaaa\0"
     with pytest.raises(StopIteration) as v:
-        cor.send(None)
+        run_cor(header.read_lua_value, data)
     assert v.value.value == "aaaaa"
 
     # Check invalid unicode just in case
-    gen = GeneratorData()
-    gen.data = b"\1aaaa\xc0 \0"
-    cor = header.read_lua_value(gen)
+    data = b"\1aaaa\xc0 \0"
     with pytest.raises(ValueError) as v:
-        cor.send(None)
+        run_cor(header.read_lua_value, data)
 
 
 def test_lua_dict_value():
-    gen = GeneratorData()
-    gen.data = b"\4\1a\0\1b\0\5"
-    cor = header.read_lua_value(gen)
+    data = b"\4\1a\0\1b\0\5"
     with pytest.raises(StopIteration) as v:
-        cor.send(None)
+        run_cor(header.read_lua_value, data)
     assert v.value.value == {"a": "b"}
 
-    gen = GeneratorData()
-    gen.data = b"\4\5"
-    cor = header.read_lua_value(gen)
     with pytest.raises(StopIteration) as v:
-        cor.send(None)
+        run_cor(header.read_lua_value, b"\4\5")
     assert v.value.value == {}
 
-    gen = GeneratorData()
-    gen.data = b"\4\2\4\1a\0\1b\0\5\5"
-    cor = header.read_lua_value(gen)
+
+def test_lua_nested_dict():
+    data = b"\4\2\4\1a\0\4\5\5\5"
     with pytest.raises(StopIteration) as v:
-        cor.send(None)
-    assert v.value.value == {None: {"a": "b"}}
+        run_cor(header.read_lua_value, data)
+    assert v.value.value == {None: {"a": {}}}
 
 
 def test_lua_dict_as_key():
-    gen = GeneratorData()
-    gen.data = b"\4\4\5\2\5"
-    cor = header.read_lua_value(gen)
     with pytest.raises(ValueError):
-        cor.send(None)
+        run_cor(header.read_lua_value, b"\4\4\5\2\5")
 
 
 def test_lua_unpaired_dict_end():
-    gen = GeneratorData()
-    gen.data = b"\4\5\4\2\4\5\5\5\4"
-    cor = header.read_lua_value(gen)
+    data = b"\5\4"
     with pytest.raises(ValueError):
-        cor.send(None)
+        run_cor(header.read_lua_value, data)
+
+
+def test_lua_dict_odd_item_number():
+    data = b"\4\1a\0\1b\0\1c\0\5"
+    with pytest.raises(ValueError):
+        run_cor(header.read_lua_value, data)

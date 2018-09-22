a442d0e121b66920c41004369f7db381617915c1
Konstantin Kalinovsky <konstantin.kalinovsky@firma.seznam.cz>
2018-09-23 01:05:11 +0200

    Synchronizing tags

--- docker-build.sh
+++ docker-build.sh
@@ -1,7 +1,4 @@
 #!/usr/bin/env bash
 
-VERSION=`grep "LABEL version" Dockerfile | cut -d'"' -f2`
-IMAGE_VERSION="faf-aio-replayserver:$VERSION"
-
-docker build --rm=true --tag=$IMAGE_VERSION .
-echo "Docker container has been build $IMAGE_VERSION"
+docker build --rm=true --tag=faf-aio-replayserver .
+echo "Docker container has been build 'faf-aio-replayserver'"

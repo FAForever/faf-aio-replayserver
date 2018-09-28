d7b00f53c82f6f8ba89ae27e3fb18075a8ba76ec
Igor Kotrasinski <i.kotrasinsk@gmail.com>
2018-09-28 15:22:22 +0200

    Add trivial Replays build test
    Signed-off-by: Igor Kotrasinski <i.kotrasinsk@gmail.com>

---
+++ tests/integration_tests/server/test_replays.py
@@ -0,0 +1,15 @@
+from replayserver.server.replays import Replays
+from replayserver.receive.mergestrategy import MergeStrategies
+
+
+config = {
+    "config_merger_grace_period_time": 30,
+    "config_replay_merge_strategy": MergeStrategies.GREEDY,
+    "config_sent_replay_delay": 5 * 60,
+    "config_sent_replay_position_update_interval": 1,
+    "config_replay_forced_end_time": 5 * 60 * 60,
+}
+
+
+def test_replays_init():
+    Replays.build(**config)

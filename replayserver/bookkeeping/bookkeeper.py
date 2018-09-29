26f2006da1fcd973ac98651fe83f1ecb5eb5457f
Igor Kotrasinski <i.kotrasinsk@gmail.com>
2018-09-30 01:29:03 +0200

    Add bookkeeper skeleton
    Signed-off-by: Igor Kotrasinski <i.kotrasinsk@gmail.com>

--- replayserver/bookkeeping/bookkeeper.py
+++ replayserver/bookkeeping/bookkeeper.py
@@ -1,7 +1,14 @@
-# TODO
+from replayserver.errors import BookkeepingError
+
+
 class Bookkeeper:
     def __init__(self):
         pass
 
     async def save_replay(self):
-        pass
+        # Todo - args and members
+        try:
+            self._storage.save_replay(game_id, stream)
+            self._database.register_replay(game_id)
+        except BookkeepingError:
+            pass    # TODO - log

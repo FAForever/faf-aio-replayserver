f4d18397b7c562c09e43dbb5c50156d3194bb375
Igor Kotrasinski <i.kotrasinsk@gmail.com>
2018-10-01 23:24:06 +0200

    Add a tiny facade for asyncio server
    Signed-off-by: Igor Kotrasinski <i.kotrasinsk@gmail.com>

--- replayserver/server/server.py
+++ replayserver/server/server.py
@@ -1,5 +1,4 @@
 import asyncio
-from contextlib import contextmanager
 
 from replayserver.errors import BadConnectionError
 from replayserver.server.connection import Connection, ConnectionHeader
@@ -7,48 +6,65 @@ from replayserver.server.replays import Replays
 
 
 class Server:
-    def __init__(self, server, replays, connection_builder, header_read):
-        self._server = server
+    def __init__(self, connection_producer, replays, header_read):
         self._replays = replays
-        self._connection_builder = connection_builder
+        self._connection_producer = connection_producer
         self._header_read = header_read
         self._connections = set()
 
     @classmethod
-    def build(cls, *, config_server_port, **kwargs):
-        def server(cb):
-            return asyncio.streams.start_server(cb, port=config_server_port)
+    def build(cls, **kwargs):
+        producer = ConnectionProducer.build(**kwargs)
         replays = Replays.build(**kwargs)
-        return cls(server, replays, Connection, ConnectionHeader.read)
+        return cls(producer, replays, ConnectionHeader.read)
 
     async def start(self):
         await self._replays.start()
-        # A tiny hack - we can't pass in a server directly since we have to
-        # register our method as a callback at creation
-        self._server = await self._server(self.handle_connection)
+        await self._connection_producer.start(self.handle_connection)
 
     async def stop(self):
-        self._server.close()
-        await self._server.wait_closed()
+        await self._connection_producer.stop()
         for connection in self._connections:
             connection.close()
         await self._replays.stop()
 
-    @contextmanager
-    def _get_connection(self, reader, writer):
-        connection = self._connection_builder(reader, writer)
+    async def handle_connection(self, connection):
         self._connections.add(connection)
         try:
-            yield connection
+            header = self._header_read(connection)
+            replay = await self._replays.get_matching_replay(header)
+            await replay._handle_connection(header, connection)
+        except BadConnectionError:
+            pass    # TODO - log
         finally:
             self._connections.remove(connection)
             connection.close()
 
-    async def handle_connection(self, reader, writer):
-        with self._get_connection(reader, writer) as connection:
-            try:
-                header = self._header_read(connection)
-                replay = await self._replays.get_matching_replay(header)
-                await replay._handle_connection(header, connection)
-            except BadConnectionError:
-                pass    # TODO - log
+
+class ConnectionProducer:
+    """
+    Tiny facade for an asyncio server. There's really nothing to unit test
+    here, as any unit tests boil down to 'the method does what it does'.
+    Hence, no DI.
+    """
+    def __init__(self, server_port):
+        self._server = None
+        self._server_port = server_port
+        self._callback = None
+
+    @classmethod
+    def build(cls, *, config_server_port, **kwargs):
+        return cls(config_server_port)
+
+    async def start(self, callback):
+        self._callback = callback
+        self._server = await asyncio.streams.start_server(
+            self._make_connection, port=self._server_port)
+
+    async def _make_connection(self, reader, writer):
+        connection = Connection(reader, writer)
+        await self._callback(connection)
+
+    async def stop(self):
+        self._server.close()
+        await self._server.wait_closed()

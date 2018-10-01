635e05afc16f99374b4116071fcaf33c35c3aea4
Igor Kotrasinski <i.kotrasinsk@gmail.com>
2018-10-02 00:00:14 +0200

    Split off server module from connection management
    Server class will now handle spinning up external components like
    connection accepting and database, as well as shutting down the internal
    classes, then external components.
    
    Signed-off-by: Igor Kotrasinski <i.kotrasinsk@gmail.com>

--- replayserver/server/server.py
+++ replayserver/server/server.py
@@ -6,27 +6,38 @@ from replayserver.server.replays import Replays
 
 
 class Server:
-    def __init__(self, connection_producer, replays, header_read):
+    def __init__(self, connection_producer, connections, replays):
         self._replays = replays
         self._connection_producer = connection_producer
+        self._connections = connections
+
+    @classmethod
+    def build(cls, **kwargs):
+        replays = Replays.build(**kwargs)
+        conns = Connections.build(**kwargs)
+        producer = ConnectionProducer.build(conns.handle_connection, **kwargs)
+        return cls(producer, replays, conns)
+
+    async def start(self):
+        await self._replays.start()
+        await self._connection_producer.start(
+            self.connections.handle_connection)
+
+    async def stop(self):
+        await self._connection_producer.stop()
+        self._connections.close_all()
+        await self._replays.stop_all()
+
+
+class Connections:
+    def __init__(self, header_read, replays):
+        self._replays = replays
         self._header_read = header_read
         self._connections = set()
 
     @classmethod
-    def build(cls, **kwargs):
-        producer = ConnectionProducer.build(**kwargs)
-        replays = Replays.build(**kwargs)
-        return cls(producer, replays, ConnectionHeader.read)
-
-    async def start(self):
-        await self._replays.start()
-        await self._connection_producer.start(self.handle_connection)
-
-    async def stop(self):
-        await self._connection_producer.stop()
-        for connection in self._connections:
-            connection.close()
-        await self._replays.stop_all()
+    def build(cls, replays, **kwargs):
+        return cls(replays, ConnectionHeader.read)
 
     async def handle_connection(self, connection):
         self._connections.add(connection)
@@ -40,6 +51,10 @@ class Server:
             self._connections.remove(connection)
             connection.close()
 
+    def close_all(self):
+        for connection in self._connections:
+            connection.close()
+
 
 class ConnectionProducer:
     """
@@ -47,17 +62,16 @@ class ConnectionProducer:
     here, as any unit tests boil down to 'the method does what it does'.
     Hence, no DI.
     """
-    def __init__(self, server_port):
+    def __init__(self, callback, server_port):
         self._server = None
         self._server_port = server_port
-        self._callback = None
+        self._callback = callback
 
     @classmethod
-    def build(cls, *, config_server_port, **kwargs):
-        return cls(config_server_port)
+    def build(cls, callback, *, config_server_port, **kwargs):
+        return cls(callback, config_server_port)
 
-    async def start(self, callback):
-        self._callback = callback
+    async def start(self):
         self._server = await asyncio.streams.start_server(
             self._make_connection, port=self._server_port)
 

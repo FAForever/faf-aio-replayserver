90e6a3acba472b625c255ae24fbae68681d3352b
Igor Kotrasinski <i.kotrasinsk@gmail.com>
2018-10-12 21:18:08 +0200

    Update and improve entry point
    Signed-off-by: Igor Kotrasinski <i.kotrasinsk@gmail.com>

--- replayserver/main.py
+++ replayserver/main.py
@@ -1,20 +1,32 @@
 import asyncio
+from os.environ import get as eget
 from replayserver import Server
 from replayserver.receive.mergestrategy import MergeStrategies
 
+__all__ = ["main"]
 
 config = {
-    "merger_grace_period_time": 30,
+    "merger_grace_period_time": eget("REPLAY_GRACE_PERIOD", 30),
     "replay_merge_strategy": MergeStrategies.FOLLOW_STREAM,
-    "sent_replay_delay": 5 * 60,
+    "sent_replay_delay": eget("REPLAY_DELAY", 5 * 60),
     "sent_replay_position_update_interval": 1,
-    "replay_forced_end_time": 5 * 60 * 60,
-    "server_port": 15000,
+    "replay_forced_end_time": eget("REPLAY_FORCE_END_TIME", 5 * 60 * 60),
+    "server_port": eget("PORT", 15000),
+    "db_host": eget("MYSQL_HOST", None),
+    "db_port": eget("MYSQL_PORT", None),
+    "db_user": eget("MYSQL_USER", None),
+    "db_password": eget("MYSQL_PASSWORD", None),
+    "db_name": eget("MYSQL_DB", None),
+    "replay_store_path": eget("REPLAY_DIR", None)
 }
 config = {"config_" + k: v for k, v in config.items()}
 
 
 def main():
+    for key in config:
+        if config[key] is None:
+            return 1
+
     server = Server.build(**config)
     loop = asyncio.get_event_loop()
     asyncio.ensure_future(server.start())

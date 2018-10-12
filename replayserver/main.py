69e6aa9e1d3fd54ce42035609dec11a6b35dbb74
Igor Kotrasinski <i.kotrasinsk@gmail.com>
2018-10-12 22:19:10 +0200

    Config dict fixups
    Signed-off-by: Igor Kotrasinski <i.kotrasinsk@gmail.com>

--- replayserver/main.py
+++ replayserver/main.py
@@ -15,31 +15,30 @@ def eget(*args, **kwargs):
 
 def main():
     env_config = {
-        "merger_grace_period_time": ("REPLAY_GRACE_PERIOD", 30),
-        "replay_merge_strategy": ("REPLAY_MERGE_STRATEGY", "FOLLOW_STREAM"),
-        "sent_replay_delay": ("REPLAY_DELAY", 5 * 60),
-        "replay_forced_end_time": ("REPLAY_FORCE_END_TIME", 5 * 60 * 60),
-        "server_port": ("PORT", 15000),
-        "db_host": ("MYSQL_HOST", None),
-        "db_port": ("MYSQL_PORT", None),
-        "db_user": ("MYSQL_USER", None),
-        "db_password": ("MYSQL_PASSWORD", None),
-        "db_name": ("MYSQL_DB", None),
-        "replay_store_path": ("REPLAY_DIR", None)
+        "config_merger_grace_period_time": ("REPLAY_GRACE_PERIOD", 30),
+        "config_replay_merge_strategy": ("REPLAY_MERGE_STRATEGY", "FOLLOW_STREAM"),
+        "config_sent_replay_delay": ("REPLAY_DELAY", 5 * 60),
+        "config_replay_forced_end_time": ("REPLAY_FORCE_END_TIME", 5 * 60 * 60),
+        "config_server_port": ("PORT", 15000),
+        "config_db_host": ("MYSQL_HOST", None),
+        "config_db_port": ("MYSQL_PORT", None),
+        "config_db_user": ("MYSQL_USER", None),
+        "config_db_password": ("MYSQL_PASSWORD", None),
+        "config_db_name": ("MYSQL_DB", None),
+        "config_replay_store_path": ("REPLAY_DIR", None)
     }
 
     config = {
-        "sent_replay_position_update_interval": 1,
+        "config_sent_replay_position_update_interval": 1,
     }
-
     config.update({k: eget(*v) for k, v in env_config.items()})
-    config = {"config_" + k: v for k, v in config.items()}
 
     logger.setLevel(eget("LOG_LEVEL", logging.INFO))
     for key in config:
         if config[key] is None:
-            logger.critical((f"Missing config key: {key}"
-                             f"Set it using env var {config[key][0]}."))
+            logger.critical((f"Missing config key: {key}. "
+                             f"Set it using env var {env_config[key][0]}."))
+            return 1
 
     strat_str = config["config_replay_merge_strategy"]
     try:

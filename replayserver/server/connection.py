d3fd7030d950fe970ba72a188ebd83a357ba54a1
Igor Kotrasinski <i.kotrasinsk@gmail.com>
2018-10-13 13:00:35 +0200

    Don't drain writer after every write
    It shouldn't be necessary, might hurt performance and all tests pass
    without it.
    
    Signed-off-by: Igor Kotrasinski <i.kotrasinsk@gmail.com>

--- replayserver/server/connection.py
+++ replayserver/server/connection.py
@@ -27,7 +27,6 @@ class Connection:
 
     async def write(self, data):
         self.writer.write(data)
-        await self.writer.drain()   # TODO - might hurt performance?
 
     def close(self):
         self.writer.transport.abort()   # Drop connection immediately

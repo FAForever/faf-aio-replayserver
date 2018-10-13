3d24a58ef268aa16fd1eaaa83b2698c0b2baf5c9
Igor Kotrasinski <i.kotrasinsk@gmail.com>
2018-10-13 12:31:12 +0200

    Convert string types from env
    Signed-off-by: Igor Kotrasinski <i.kotrasinsk@gmail.com>

--- replayserver/main.py
+++ replayserver/main.py
@@ -14,38 +14,41 @@ def eget(*args, **kwargs):
 
 
 def main():
-    env_config = {
-        "config_merger_grace_period_time": ("REPLAY_GRACE_PERIOD", 30),
-        "config_replay_merge_strategy": ("REPLAY_MERGE_STRATEGY", "FOLLOW_STREAM"),
-        "config_sent_replay_delay": ("REPLAY_DELAY", 5 * 60),
-        "config_replay_forced_end_time": ("REPLAY_FORCE_END_TIME", 5 * 60 * 60),
-        "config_server_port": ("PORT", 15000),
-        "config_db_host": ("MYSQL_HOST", None),
-        "config_db_port": ("MYSQL_PORT", None),
-        "config_db_user": ("MYSQL_USER", None),
-        "config_db_password": ("MYSQL_PASSWORD", None),
-        "config_db_name": ("MYSQL_DB", None),
-        "config_replay_store_path": ("REPLAY_DIR", None)
+    logger.setLevel(eget("LOG_LEVEL", logging.INFO))
+
+    env_config_source = {
+        "config_merger_grace_period_time": ("REPLAY_GRACE_PERIOD", 30, int),
+        "config_replay_merge_strategy": ("REPLAY_MERGE_STRATEGY", "FOLLOW_STREAM", MergeStrategies),
+        "config_sent_replay_delay": ("REPLAY_DELAY", 5 * 60, int),
+        "config_replay_forced_end_time": ("REPLAY_FORCE_END_TIME", 5 * 60 * 60, int),
+        "config_server_port": ("PORT", 15000, int),
+        "config_db_host": ("MYSQL_HOST", None, str),
+        "config_db_port": ("MYSQL_PORT", None, int),
+        "config_db_user": ("MYSQL_USER", None, str),
+        "config_db_password": ("MYSQL_PASSWORD", None, str),
+        "config_db_name": ("MYSQL_DB", None, str),
+        "config_replay_store_path": ("REPLAY_DIR", None, str)
     }
+
+    env_config = {}
+    for k, v in env_config_source.items():
+        env_name, env_default, env_type = v
+        env_config[k] = eget(env_name, env_default)
+        if env_config[k] is None:
+            logger.critical((f"Missing config key: {k}. "
+                             f"Set it using env var {env_name}."))
+            return 1
+        try:
+            env_config[k] = v[2](env_config[k])
+        except ValueError as e:
+            logger.critical(f"Invalid value for {k} ({env_name}) - "
+                            f"{str(e)}")
+            return 1
 
     config = {
         "config_sent_replay_position_update_interval": 1,
     }
-    config.update({k: eget(*v) for k, v in env_config.items()})
-
-    logger.setLevel(eget("LOG_LEVEL", logging.INFO))
-    for key in config:
-        if config[key] is None:
-            logger.critical((f"Missing config key: {key}. "
-                             f"Set it using env var {env_config[key][0]}."))
-            return 1
-
-    strat_str = config["config_replay_merge_strategy"]
-    try:
-        config["config_replay_merge_strategy"] = MergeStrategies(strat_str)
-    except ValueError:
-        logger.critical(f"{strat_str} is not a valid replay merge strategy")
-        return 1
+    config.update(env_config)
 
     server = Server.build(**config)
     loop = asyncio.get_event_loop()

bc2cf066ea53106c3c4d1e7935147e1c57b9abab
Igor Kotrasinski <i.kotrasinsk@gmail.com>
2018-10-14 18:42:11 +0200

    Log to stderr
    Signed-off-by: Igor Kotrasinski <i.kotrasinsk@gmail.com>

--- replayserver/logging.py
+++ replayserver/logging.py
@@ -1,3 +1,7 @@
 import logging
 
 logger = logging.getLogger("replayserver")
+stderr_handler = logging.StreamHandler()
+stderr_handler.setFormatter(logging.Formatter(
+    '%(asctime)s %(levelname)-8s %(message)s'))
+logger.addHandler(stderr_handler)

59002a772a278218dd6ba71912265ee6aa16b139
Igor Kotrasinski <i.kotrasinsk@gmail.com>
2018-11-01 01:09:38 +0100

    Rewrite "follow stream" strategy
    Move logic for comparing data only once to a separate class. Rewrite
    strategy so that its invariants are clearer.
    
    Signed-off-by: Igor Kotrasinski <i.kotrasinsk@gmail.com>

--- replayserver/receive/mergestrategy.py
+++ replayserver/receive/mergestrategy.py
@@ -64,6 +64,31 @@ class GreedyMergeStrategy(MergeStrategy):
         self.sink_stream.finish()
 
 
+class DivergenceTracking:
+    """
+    Allows us to compare stream with sink for divergence. Ensures that we never
+    compare the same data twice.
+    """
+    def __init__(self, stream, sink):
+        self._stream = stream
+        self._sink = sink
+        self.diverges = False
+        self._compared_num = 0
+
+    def check_divergence(self):
+        if self.diverges:
+            return
+        start = self._compared_num
+        end = min(len(self._stream.data), len(self._sink.data))
+        if start >= end:
+            return
+        # This is safe since we don't await with the memoryviews
+        with memoryview(self._stream.data.bytes()) as view1, \
+                memoryview(self._sink.data.bytes()) as view2:
+            self.diverges = view1[start:end] != view2[start:end]
+        self._compared_num = end
+
+
 class FollowStreamMergeStrategy(MergeStrategy):
     """
     Tries its best to follow a single stream. If it ends, looks for a new
@@ -77,93 +102,72 @@ class FollowStreamMergeStrategy(MergeStrategy):
 
     Invariants:
     0. Streams match iif one's data is a prefix of the other's.
-    1. Tracked stream MUST match sink at the moment of feeding data.
-    2. If stream is set as tracked, it MUST match sink at that moment.
-    3. Matching set MUST contain all matching streams at all times.
-    4. Matching set MUST contain tracked stream at all times.
-    5. Matching set MAY contain diverging streams, but all streams in matching
-       set MUST match sink at moment of adding.
-    6. After finalize(), ALL streams either diverge or are prefices of sink.
+    1. Tracked stream always matches and has at least as much data as sink.
+    2. We don't track a stream iif no stream fits above condition.
+    3. Matching set MUST contain all matching streams.
+    4. Matching set MAY contain diverging streams. They are lazily removed
+       when we check if a stream is fit to be tracked.
+    5. After finalize(), ALL streams either diverge or are prefices of sink.
     """
     def __init__(self, sink_stream):
         MergeStrategy.__init__(self, sink_stream)
-        # We keep info on where to we compared a stream to sink last
-        self._matching_streams = {}
-        self._tracked_stream = None
+        self._matching = {}
+        self._tracked = None
 
     @classmethod
     def build(cls, sink_stream, **kwargs):
         return cls(sink_stream)
 
-    def _matching(self, stream):
-        return stream in self._matching_streams
+    def _is_ahead_of_sink(self, stream):
+        return len(stream.data) > len(self.sink_stream.data)
 
-    def _cmp_end(self, stream):
-        return min(len(stream.data), len(self.sink_stream.data))
-
-    def _matches_sink(self, stream):
-        start = self._matching_streams[stream]
-        end = self._cmp_end(stream)
-        if start >= end:
-            return True
-        # This is safe since we don't await with the memoryviews
-        with memoryview(stream.data.bytes()) as view1, \
-                memoryview(self.sink_stream.data.bytes()) as view2:
-            return view1[start:end] == view2[start:end]
+    def _fits_tracking_conditions(self, stream):
+        if not self._is_ahead_of_sink(stream):
+            return False
+        self._check_for_divergence(stream)
+        return stream in self._matching
 
     def _check_for_divergence(self, stream):
-        if self._matches_sink(stream):
-            self._matching_streams[stream] = self._cmp_end(stream)
-        else:
-            del self._matching_streams[stream]
+        check = self._matching[stream]
+        check.check_divergence()
+        if check.diverges:
+            del self._matching[stream]
 
-    def _feed_tracked_data(self):
-        tracked = self._tracked_stream
-        if tracked is None:
+    def _feed_sink(self):
+        if self._tracked is None:
             return
         sink_len = len(self.sink_stream.data)
-        if len(tracked.data) <= sink_len:
-            return
-
-        # In case the stream diverged before it reached sink length
-        self._check_for_divergence(tracked)
-        if not self._matching(tracked):
-            # Won't happen if we're called from _find_new_stream,
-            # so no worries about recursion here
-            self._find_new_stream()
-            return
-        self.sink_stream.feed_data(tracked.data[sink_len:])
+        self.sink_stream.feed_data(self._tracked.data[sink_len:])
 
     def _find_new_stream(self):
-        self._tracked_stream = None
-        for stream in list(self._matching_streams.keys()):
-            self._check_for_divergence(stream)
-            if self._matching(stream):
-                self._tracked_stream = stream
-        self._feed_tracked_data()
+        for stream in list(self._matching.keys()):
+            if self._fits_tracking_conditions(stream):
+                self._tracked = stream
+                break
+        self._feed_sink()
 
     def stream_added(self, stream):
-        self._matching_streams[stream] = 0  # No data for now, so it matches
-        if self._tracked_stream is None:
-            self._tracked_stream = stream
-            self._feed_tracked_data()
+        self._matching[stream] = DivergenceTracking(stream, self.sink_stream)
 
     def stream_removed(self, stream):
         # Don't remove a not-tracked stream - it might have more data that
         # matches currently tracked stream, in case it ends short!
-        if stream is self._tracked_stream:
-            self._matching_streams.pop(stream, None)
+        if stream is self._tracked:
+            self._tracked = None
+            self._matching.pop(stream, None)
             self._find_new_stream()
 
     def new_data(self, stream):
-        if stream is self._tracked_stream:
-            self._feed_tracked_data()
+        if self._tracked is None and self._fits_tracking_conditions(stream):
+            self._tracked = stream
+        if stream is self._tracked:
+            self._feed_sink()
 
     def finalize(self):
         # Check any ended streams we saved for later
-        while self._matching_streams:
-            self._feed_tracked_data()
-            self.stream_removed(self._tracked_stream)
+        while self._tracked is not None:
+            self.stream_removed(self._tracked)
+        self._matching.clear()
         self.sink_stream.finish()
 
     def new_header(self, stream):

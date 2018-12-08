cb0552ef7dd3e7a37cbfc612ba6274625e571224
Igor Kotrasinski <i.kotrasinsk@gmail.com>
2018-12-08 14:18:14 +0100

    Bump version
    Signed-off-by: Igor Kotrasinski <i.kotrasinsk@gmail.com>

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ hopefully is better designed than the old replay server.
 
 setup(
     name="faf_replay_server",
-    version='0.5.6',
+    version='0.5.7',
     description="FAF replay server using asyncio",
     long_description=long_desc,
     classifiers=[

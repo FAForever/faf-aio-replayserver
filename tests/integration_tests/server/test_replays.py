1d2c1bb4ce908e774c52f6b15a7445743f60af74
Igor Kotrasinski <i.kotrasinsk@gmail.com>
2019-02-01 22:03:09 +0100

    Fix replay integration tests
    Signed-off-by: Igor Kotrasinski <i.kotrasinsk@gmail.com>

--- tests/integration_tests/server/test_replays.py
+++ tests/integration_tests/server/test_replays.py
@@ -1,16 +1,29 @@
 from replayserver.server.replays import Replays
-from replayserver.receive.mergestrategy import MergeStrategies
+from replayserver.server.replay import ReplayConfig
+from tests import config_from_dict
 
 
-config = {
-    "config_merger_grace_period_time": 30,
-    "config_replay_merge_strategy": MergeStrategies.FOLLOW_STREAM,
-    "config_mergestrategy_stall_check_period": 60,
-    "config_sent_replay_delay": 5 * 60,
-    "config_sent_replay_position_update_interval": 1,
-    "config_replay_forced_end_time": 5 * 60 * 60,
+config_dict = {
+    "forced_end_time": 5 * 60 * 60,
+    "send": {
+        "replay_delay": 5 * 60,
+        "update_interval": 1,
+    },
+    "merge": {
+        "strategy": "FOLLOW_STREAM",
+        "grace_period": 30,
+        "strategy_config": {
+            "follow_stream": {
+                "stall_check_period": 60
+            }
+        }
+    }
 }
 
 
+def replay_config(d):
+    return ReplayConfig(config_from_dict(d))
+
+
 def test_replays_init(mock_bookkeeper):
-    Replays.build(mock_bookkeeper, **config)
+    Replays.build(mock_bookkeeper, replay_config(config_dict))

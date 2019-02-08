f0c11ccbe4d0104cc61b3a793153d6cc140480e2
Igor Kotrasinski <i.kotrasinsk@gmail.com>
2019-02-09 00:23:59 +0100

    Adjust sender tests after replay lifetime rework
    Signed-off-by: Igor Kotrasinski <i.kotrasinsk@gmail.com>

--- tests/unit_tests/send/test_sender.py
+++ tests/unit_tests/send/test_sender.py
@@ -9,46 +9,68 @@ from replayserver.errors import MalformedDataError, \
     CannotAcceptConnectionError
 
 
+# FIXME - if we moved replay handling 'meat' to another class, these tests
+# would become much simpler
 @pytest.mark.asyncio
 @timeout(0.1)
-async def test_sender_doesnt_end_while_connection_runs(
-        mock_connections, outside_source_stream, locked_mock_coroutines,
-        mock_replay_headers, event_loop):
+async def test_sender_connection_doesnt_end_until_stream_ends(
+        mock_connections, outside_source_stream, mock_replay_headers,
+        event_loop):
     mock_header = mock_replay_headers()
     connection = mock_connections()
     outside_source_stream.feed_data(b"aaaaa")
     outside_source_stream.set_header(mock_header)
-    end, coro = locked_mock_coroutines()
-    connection.write.side_effect = coro
+
+    sender = Sender(outside_source_stream)
+    h = asyncio.ensure_future(sender.handle_connection(connection))
+    await exhaust_callbacks(event_loop)
+    assert not h.done()
+
+    outside_source_stream.finish()
+    await h
+    sender.stop_accepting_connections()
+    await sender.wait_for_ended()
+
+
+@pytest.mark.asyncio
+@timeout(0.1)
+async def test_sender_doesnt_end_while_connection_runs(
+        mock_connections, outside_source_stream, mock_replay_headers,
+        event_loop):
+    mock_header = mock_replay_headers()
+    connection = mock_connections()
+    outside_source_stream.feed_data(b"aaaaa")
+    outside_source_stream.set_header(mock_header)
 
     sender = Sender(outside_source_stream)
     h = asyncio.ensure_future(sender.handle_connection(connection))
     w = asyncio.ensure_future(sender.wait_for_ended())
     await exhaust_callbacks(event_loop)
-    outside_source_stream.finish()
+    sender.stop_accepting_connections()
     await exhaust_callbacks(event_loop)
     assert not w.done()
-    end.set()
+
+    outside_source_stream.finish()
     await h
     await w
 
 
 @pytest.mark.asyncio
 @timeout(0.1)
-async def test_sender_ends_with_ended_stream_and_no_connections(
+async def test_sender_ends_when_refusing_conns_and_no_connections(
         outside_source_stream):
     sender = Sender(outside_source_stream)
-    outside_source_stream.finish()
+    sender.stop_accepting_connections()
     await sender.wait_for_ended()
 
 
 @pytest.mark.asyncio
 @timeout(0.1)
-async def test_sender_refuses_connection_after_stream_ends(
+async def test_sender_refuses_connection_when_told_to(
         mock_connections, outside_source_stream, event_loop):
     connection = mock_connections()
     sender = Sender(outside_source_stream)
-    outside_source_stream.finish()
+    sender.stop_accepting_connections()
     with pytest.raises(CannotAcceptConnectionError):
         await sender.handle_connection(connection)
     await sender.wait_for_ended()
@@ -67,6 +89,7 @@ async def test_sender_no_header(mock_connections, outside_source_stream,
         await f
 
     connection.write.assert_not_called()
+    sender.stop_accepting_connections()
     await sender.wait_for_ended()
 
 
@@ -80,10 +103,9 @@ async def test_sender_connection_calls(mock_connections, outside_source_stream,
     sender = Sender(outside_source_stream)
     outside_source_stream.set_header(mock_header)
     outside_source_stream.feed_data(b"Data")
-    f = asyncio.ensure_future(sender.handle_connection(connection))
-    await exhaust_callbacks(event_loop)
     outside_source_stream.finish()
-    await f
+    await sender.handle_connection(connection)
     connection.write.assert_has_awaits([asynctest.call(b"Header"),
                                         asynctest.call(b"Data")])
+    sender.stop_accepting_connections()
     await sender.wait_for_ended()
